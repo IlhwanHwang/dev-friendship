7c092db5cf2b859350d3703add49ab2a9ec1e368
Ilhwan Hwang <ilhwan.hwang@bagelcode.com>
2019-07-10 11:52:28 +0900

    Revise ChoiceCell design

--- src/ChoiceCell.tsx
+++ src/ChoiceCell.tsx
@@ -23,71 +23,62 @@ export default class ChoiceCell extends React.Component {
   }
 
   render = () => {
-    const column = `col-${Math.floor(12 / this.props.portion)}`
+    // const column = `col-${Math.floor(12 / this.props.portion)}`
     return (
       <div
-        className={`${column} d-flex justify-content-center ${ this.props.imageUrl.length > 0 ? "align-items-end" : "align-items-center" }`}
-        style={{
-          height: "16rem",
-          backgroundColor: "white"
-        }}
+        className="col-12 p-0"
         onMouseOver={() => { this.setState({ entered: true }) }}
         onMouseOut={() => { this.setState({ entered: false }) }}
       >
         <div
-          className="w-100 h-100 d-flex justify-content-center align-items-center"
-          style={{ zIndex: 0, position: "absolute" }}
-        >
-          {
-            <img
-              className={ this.props.imageUrl.endsWith(".svg") ? "w-75 h-75" : "w-100 h-100" }
-              src={this.props.imageUrl || this.imageFallback}
-              style={{ objectFit: this.props.imageUrl.endsWith(".svg") ? "scale-down" : "cover" }}
-            >
-            </img>
-          }
-        </div>
-        <div
-          className="w-100 h-100"
+          className="w-100 h-100 pb-0 pl-3 pr-3 pt-3 d-flex"
           style={{
-            zIndex: 1,
-            position: "absolute",
-            backgroundColor: "black",
-            opacity: this.props.enable && this.state.entered ? 0.3 : 0.0,
-          }}
-        ></div>
-        <span
-          className="pl-2 pr-2"
-          style={{
-            zIndex: 2,
-            fontSize: "1.5rem",
-            textShadow: "0px 0px 5px black",
-            textAlign: "center",
-            color: "white",
-            backgroundColor: "#0008",
-            borderRadius: "5px",
-            fontFamily: this.props.imageUrl.length > 0 ? "inherit" : "monospace"
-          }}
-        >
-          {this.props.text}
-        </span>
-        <div
-          className="w-100 h-100"
-          style={{
-            zIndex: 3,
-            position: "absolute",
-            backgroundColor: (this.props.cellStyle === "incorrect" ? "#f008" : (this.props.cellStyle === "correct" ? "#0f08" : ""))
-          }}
-        >
-        </div>
-        <div
-          className="w-100 h-100"
-          style={{
-            zIndex: 4,
-            position: "absolute"
+            height: "4rem",
+            backgroundColor: (this.props.cellStyle === "incorrect" ? "#f008" : (this.props.cellStyle === "correct" ? "#0f08" : "")),
           }}
           onClick={this.props.enable ? this.props.onClick : () => {}}
         >
+          <div
+            className="flex-grow-1 d-flex"
+            style={{
+              boxShadow: "0rem 0.125rem 0.5rem #0002"
+            }}
+          >
+            <div
+              className="d-flex justify-content-center align-items-center"
+              style={{
+                zIndex: 0,
+                width: "4rem",
+                height: "4rem",
+              }}
+            >
+              {
+                <img
+                  className={ this.props.imageUrl.endsWith(".svg") ? "w-75 h-75" : "w-100 h-100" }
+                  src={this.props.imageUrl || this.imageFallback}
+                  style={{ objectFit: this.props.imageUrl.endsWith(".svg") ? "scale-down" : "cover" }}
+                >
+                </img>
+              }
+            </div>
+            <div
+              className="flex-grow-1"
+              style={{
+                backgroundColor: "#fff"
+              }}
+            >
+              <span
+                className="pl-2 pr-2"
+                style={{
+                  zIndex: 2,
+                  fontSize: "1.5rem",
+                  fontFamily: this.props.imageUrl.length > 0 ? "inherit" : "monospace"
+                }}
+              >
+                {this.props.text}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     )
