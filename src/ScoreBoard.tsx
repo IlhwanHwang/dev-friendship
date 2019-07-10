7d5e34647e20b55b4f105931d62bcbebb76aef08
Ilhwan Hwang <ilhwan.hwang@bagelcode.com>
2019-07-10 17:53:58 +0900

    Change style of scoreboard

--- src/ScoreBoard.tsx
+++ src/ScoreBoard.tsx
@@ -1,6 +1,67 @@
 import * as React from 'react'
 import * as api from './api'
 
+class ScoreBoardElem extends React.Component {
+  props: {
+    name: string,
+    score: number,
+    rank: number,
+    highlit: boolean
+  }
+
+  render = () => {
+    return (
+      <div className="col-12 p-0">
+        <div
+          className="w-100 h-100 pb-0 pl-3 pr-3 pt-3 d-flex"
+          style={{ height: "4rem" }}
+        >
+          <div
+            className="flex-grow-1 d-flex"
+            style={{ boxShadow: "0rem 0.125rem 0.5rem #0002" }}
+          >
+            <div
+              className="d-flex justify-content-center align-items-center"
+              style={{
+                zIndex: 0,
+                width: "4rem",
+                height: "4rem",
+                backgroundColor: this.props.rank === 1 ? "gold" : (this.props.rank === 2 ? "silver" : (this.props.rank === 3 ? "#cd7f32" : ""))
+              }}
+            >
+              <span style={{
+                fontSize: "2rem"
+              }}>
+                {this.props.score}
+              </span>
+            </div>
+            <div
+              className="flex-grow-1 pl-2 pt-1"
+              style={{
+                backgroundColor: "#fff"
+              }}
+            >
+              <span
+                className="pl-2 pr-2 bg-dark text-white"
+                style={{
+                  fontSize: "1.3rem",
+                  borderRadius: "0.5rem"
+                }}
+              >
+                {this.props.name}
+              </span>
+              <br></br>
+              <span className="text-secondary">
+                {this.props.rank}등
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export default class ScoreBoard extends React.Component {
   props: {
     userId: string,
@@ -37,17 +98,15 @@ export default class ScoreBoard extends React.Component {
       return (
         <div>
           {
-            this.scoreBoard.map(entry => {
-              if (this.props.highlight === entry.userId) {
-                return (
-                  <strong key={entry.userId}>{entry.name}: {entry.score}<br></br></strong>
-                )
-              }
-              else {
-                return (
-                  <span key={entry.userId}>{entry.name}: {entry.score}<br></br></span>
-                )
-              }
+            this.scoreBoard.map((entry, index) => {
+              return (
+                <ScoreBoardElem
+                  name={entry.name}
+                  score={entry.score}
+                  rank={index + 1}
+                  highlit={entry.userId === this.props.userId}
+                />
+              )
             })
           }
         </div>
